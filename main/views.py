25788c831d50cf31dc014581e68c05d4cba9b869
Nariman <Nariman079i>
2022-11-07 06:09:45 +0300

    edit app

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    file = 'main/index.html'
+    context = {
+        'title':'Артур чмо'
+    }
+    return render(request,file,context=context)
