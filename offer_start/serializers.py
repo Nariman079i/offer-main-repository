8b519a32bad0653ec9b1cf57d03ede85aa3dd377
Nariman <Nariman079i>
2022-12-04 18:09:19 +0300

    create api for person account

--- offer_start/serializers.py
+++ offer_start/serializers.py
@@ -17,14 +17,41 @@ class InvestorSerializer(ModelSerializer):
         model = Investor
         fields = '__all__'
 
+    def validate(self,data):
+        errors = {}
+        text = 'Поле не должно быть пустым'
+        for k in data:
+            if not data[k] or data[k] == "":
+                errors[k] = text
+                raise ValidationError({k:text})
+        return data
+
 class BusinessmanSerializer(ModelSerializer):
     user = HiddenField(default=CurrentUserDefault())
     class Meta:
         model = Businessman
         fields = "__all__"
 
+    def validate(self, data):
+        errors = {}
+        text = 'Поле не должно быть пустым'
+        for k in data:
+            if not data[k] or data[k] == "":
+                errors[k] = text
+                raise ValidationError({k: text})
+        return data
+
 class CompanySerializer(ModelSerializer):
     user = HiddenField(default=CurrentUserDefault())
     class Meta:
         model = Company
         fields = '__all__'
+
+    def validate(self,data):
+        errors = {}
+        text = 'Поле не должно быть пустым'
+        for k in data:
+            if not data[k] or data[k] == "":
+                errors[k] = text
+                raise ValidationError({k:text})
+        return data
